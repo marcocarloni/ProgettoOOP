100915ba316f12da202451762b5e19795c370eda
matteocardinali22 <icardinals22@gmail.com>
2019-07-08 11:25:55 +0200

    Creazione classe studente

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,12 @@
+package model;
+import java.io.Serializable;
+/**
+Classe modellante dei dati contenente gli attributi presenti nel file .csv
+In particolare questa è la classe modellante dei dati riferiti ai vari studenti
+che hanno deciso di fare un'esperienza di studio/lavoro all'estero.	
+*/
+public class Student implements Serializable
+{
+	private int age;
+	
+}
