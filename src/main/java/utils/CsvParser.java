554781a6efa31ecf10bd03e9787e883f035e0ef5
matteocardinali22 <icardinals22@gmail.com>
2019-07-09 15:48:06 +0200

    Inizializzazione classe parser

---
+++ src/main/java/utils/CsvParser.java
@@ -0,0 +1,18 @@
+package utils;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.*;
+import model.ErasmusData;
+import org.springframework.boot.json.BasicJsonParser;
+
+
+/**
+ * Classe per la gestione dei dati che vengono ricevuti al primo avvio partendo dal
+ * file json assegnato:dal json ci si riconduce all'url contenente il file .csv d' interesse.
+ */
+
+public class CsvParser 
+{
+  
+}
